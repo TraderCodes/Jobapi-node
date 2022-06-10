610025a69020318d7160504e718136930335eec0
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-10 11:43:19 -0700

    Refactor hashing , setting it in user.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,12 +6,8 @@ const bcrypt = require('bcryptjs');
 const register = async (req, res) => {
   const { password, name, email } = req.body;
 
-  // hashing password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  const tempUser = { name, email, password: hashedPassword };
 
-  const user = await User.create({ ...tempUser });
+  const user = await User.create({ ...req.body});
   res.status(StatusCodes.CREATED).json(user);
 };
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -23,7 +24,12 @@ const UserSchema = new mongoose.Schema({
     type: String,
     require: [true, 'Please provide name'],
     minlength: 6,
- 
   },
+});
+//
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
 });
 module.exports = mongoose.model('User', UserSchema);
