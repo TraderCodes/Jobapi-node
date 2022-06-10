3465520581f7864f6915aac80b9714c6c66720ff
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-10 12:10:20 -0700

    regiter controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,12 @@
 // get models
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const jwt = require('jsonwebtoken');
+
 
 const bcrypt = require('bcryptjs');
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  const token = json.sign({userId:user._id,name:user.name},'jewSecret',{expiresIn:'30d'})
+const token =  user.createJWT()
   res.status(StatusCodes.CREATED).json({user:{name:user.name},token});
 };
 const login = async (req, res) => {
